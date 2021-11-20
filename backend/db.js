95fe7fc77ad8f5fe56db137c7628b32679e97a05
Nabhan Abdulla P V <nabhan@criodo.com>
2021-11-20 05:37:26 +0000

    Update package.json, __CRIO__/metadata.json files Deleted src/App.js, src/index.css, src/index.js, src/setupTests.js, .gitignore, package-lock.json, setup.sh, src/theme.js, src/ipConfig.json, src/components/Register.js, src/components/Register.css, src/components/Login.js, src/components/Login.css, src/components/Header.js, src/components/Header.css, src/components/Footer.js, src/components/Footer.css, backend/db/products.db, backend/routes/auth.js, backend/routes/cart.js, backend/routes/product.js, backend/routes/user.js, backend/.gitignore, backend/config.json, backend/db.js, backend/index.js, backend/package-lock.json, backend/package.json, backend/utils.js, public/avatar.png, public/favicon.ico, public/index.html, public/manifest.json, public/robots.txt files

--- backend/db.js
+++
@@ -1,6 +0,0 @@
-const nedb = require('nedb');
-const users = new nedb({ filename: 'db/users.db', autoload: true })
-const products = new nedb({ filename: 'db/products.db', autoload: true });
-
-module.exports.users = users
-module.exports.products = products
--- setup.sh
+++
@@ -1,4 +0,0 @@
-# Store workspace public IP to config file
-IP_VAR="$(curl http://checkip.amazonaws.com/)"
-CONFIG='{"workspaceIp": "'"$IP_VAR"'"}'
-echo $CONFIG > src/ipConfig.json

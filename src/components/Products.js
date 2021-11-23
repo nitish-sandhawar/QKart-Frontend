435ec0301b07d6729385589d58919e14a3314746
Nabhan <nabhan@criodo.com>
2021-11-24 00:54:50 +0550

    first commit

---
+++ src/components/Products.js
@@ -0,0 +1,110 @@
+import { Search, SentimentDissatisfied } from "@mui/icons-material";
+import {
+  CircularProgress,
+  Grid,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import axios from "axios";
+import { useSnackbar } from "notistack";
+import React, { useEffect, useState } from "react";
+import { config } from "../App";
+import Footer from "./Footer";
+import Header from "./Header";
+import "./Products.css";
+
+
+const Products = () => {
+
+  /**
+   * Make API call to get the products list and store it to display the products
+   *
+   * API endpoint - "GET /products"
+   *
+   * Example for successful response from backend:
+   * HTTP 200
+   * [
+   *      {
+   *          "name": "iPhone XR",
+   *          "category": "Phones",
+   *          "cost": 100,
+   *          "rating": 4,
+   *          "image": "https://i.imgur.com/lulqWzW.jpg",
+   *          "_id": "v4sLtEcMpzabRyfx"
+   *      },
+   *      {
+   *          "name": "Basketball",
+   *          "category": "Sports",
+   *          "cost": 100,
+   *          "rating": 5,
+   *          "image": "https://i.imgur.com/lulqWzW.jpg",
+   *          "_id": "upLK9JbQ4rMhTwt4"
+   *      }
+   * ]
+   *
+   * Example for failed response from backend:
+   * HTTP 500
+   * {
+   *      "success": false,
+   *      "message": "Something went wrong. Check the backend console for more details"
+   * }
+   */
+  const performAPICall = async () => {
+  };
+
+  /**
+   * Definition for search handler
+   * This is the function that is called on adding new search keys
+   *
+   * @param {string} text
+   *    Text user types in the search bar. To filter the displayed products based on this text.
+   *
+   * API endpoint - "GET /products/search?value=<search-query>"
+   *
+   */
+  const performSearch = async (text) => {
+  };
+
+  /**
+   * Definition for debounce handler
+   * With debounce, this is the function to be called whenever the user types text in the searchbar field
+   *
+   * @param {{ target: { value: string } }} event
+   *    JS event object emitted from the search input field
+   *
+   * @param {NodeJS.Timeout} debounceTimeout
+   *    Timer id set for the previous debounce call
+   *
+   */
+  const debounceSearch = (event, debounceTimeout) => {
+  };
+
+
+
+  return (
+    <div>
+      <Header>
+
+      </Header>
+
+      <TextField
+        className="search-mobile"
+        size="small"
+        fullWidth
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <Search color="primary" />
+            </InputAdornment>
+          ),
+        }}
+        placeholder="Search for items/categories"
+        name="search"
+      />
+      <Footer />
+    </div>
+  );
+};
+
+export default Products;

9c9f25e7f4e2b3c85adb6f068e5697e8aefb7bd9
Nabhan <nabhan@criodo.com>
2021-11-27 15:13:47 +0550

    updated changes - 2021-11-27 14:53:47

--- src/components/Products.js
+++ src/components/Products.js
@@ -26,17 +26,6 @@ import "./Products.css";
  * @property {string} _id - Unique ID for the product
  */
 
-/**
- * @typedef {Object} CartItem -  - Data on product added to cart
- * 
- * @property {string} name - The name or title of the product in cart
- * @property {string} qty - The quantity of product added to cart
- * @property {string} category - The category that the product belongs to
- * @property {number} cost - The price to buy the product
- * @property {number} rating - The aggregate rating of the product (integer out of five)
- * @property {string} image - Contains URL for the product image
- * @property {string} productId - Unique ID for the product
- */
 
 const Products = () => {
 
